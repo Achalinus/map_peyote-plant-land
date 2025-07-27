0712ac73345edf3caf610f89c941af8da323104b
FreyaMoon1 <franziskaadler12@gmail.com>
2025-07-27 21:47:58 +0200

    Initial commit

---
+++ platformResourcePresets/fiveM/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'John Doe <j.doe@example.com>'
+description 'Example resource'
+version '1.0.0'description "myName"
+version "1.0.0"
+
+fx_version "cerulean"
+
+this_is_a_map "yes"
